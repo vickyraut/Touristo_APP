6ebeec59923370b566594cce3c2116b2288e88ec
Vicky Raut <vicky12cr@gmail.com>
2023-02-27 01:14:38 +0530

    Adding all initial files

--- app/src/main/java/com/polytechnic/touristo_app/Constants/Urls.java
+++
@@ -1,8 +0,0 @@
-package com.polytechnic.touristo_app.Constants;
-public class Urls {
-    public static final String OnlineAddress = "http://192.168.196.88:80/TouristoAPI/";
-
-    public static final String urlRegisterUser = OnlineAddress + "registerUser.php";
-
-    public static final String urlLoginUser = OnlineAddress + "loginUser.php";
-}
--- app/src/main/java/com/polytechnic/touristo_app/TravelLoacation.java
+++
@@ -1,8 +0,0 @@
-package com.polytechnic.touristo_app;
-
-public class TravelLoacation {
-    public  String title,location,imageUrl;
-    public Float starRating;
-
-
-}
